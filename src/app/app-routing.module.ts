041522ea4f7a3ca11090dbb81b848e14f690a43e
Prit Pambhar <prit.pambhar@aimdek.com>
2020-06-04 18:38:26 +0530

    component creation and routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,48 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GeneralInfoComponent } from './general-info/general-info.component';
+import { EducationComponent } from './education/education.component';
+import { InternshipComponent } from './internship/internship.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { CertificationsComponent } from './certifications/certifications.component';
+import { AchievementsComponent } from './achievements/achievements.component';
+import { DeclarationComponent } from './declaration/declaration.component';
 
 
 const routes: Routes = [
   {
-   path: "",
-   component: GeneralInfoComponent
-  }
+    path: "",
+    component: GeneralInfoComponent
+  },
+  {
+    path: "education",
+    component: EducationComponent
+  },
+  {
+    path: "internships",
+    component: InternshipComponent
+  },
+  {
+    path: "projects",
+    component: ProjectsComponent
+  },
+  {
+    path: "skills",
+    component: SkillsComponent
+  },
+  {
+    path: "certification",
+    component: CertificationsComponent
+  },
+  {
+    path: "achievements",
+    component: AchievementsComponent
+  },
+  {
+    path: "declaration",
+    component: DeclarationComponent
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GeneralInfoComponent } from './general-info/general-info.component';
+import { EducationComponent } from './education/education.component';
+import { InternshipComponent } from './internship/internship.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { CertificationsComponent } from './certifications/certifications.component';
+import { DeclarationComponent } from './declaration/declaration.component';
+import { AchievementsComponent } from './achievements/achievements.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GeneralInfoComponent
+    GeneralInfoComponent,
+    EducationComponent,
+    InternshipComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    CertificationsComponent,
+    DeclarationComponent,
+    AchievementsComponent
   ],
   imports: [
     BrowserModule,
